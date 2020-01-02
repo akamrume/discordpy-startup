1a3a7e990cfcc84aa1eacb1925256e4bc05dfba4
akamrume <59397190+akamrume@users.noreply.github.com>
2020-01-02 21:53:30 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -35,7 +35,7 @@ async def rect(ctx, about = "募集", cnt = 4, settime = 10.0):
 
     while len(reaction_member)-1 <= cnt:
         try:
-            reaction, user = await client.wait_for('reaction_add', timeout=settime, check=check)
+            reaction, user = await bot.wait_for('reaction_add', timeout=settime, check=check)
         except asyncio.TimeoutError:
             await ctx.send('残念、人が足りなかったようだ...')
             break
